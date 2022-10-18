f2a17dceccfe9b945ca290c4adf9ad3e7968dd55
Arjun <arjun@onlilo.in>
2022-10-18 14:22:56 +0530

    test

--- config.py
+++ config.py
@@ -6,19 +6,23 @@ Created on Tue Oct 18 12:26:47 2022
 @author: arjun
 """
 
-import os
-from pathlib import Path
-from dotenv import load_dotenv
+# import os
+# from pathlib import Path
+# from dotenv import load_dotenv
 
-# dotenv_path = Path('/home/arjun/.env')
-dotenv_path = Path('/home/ubuntu/.env')
-load_dotenv(dotenv_path=dotenv_path)
+# # dotenv_path = Path('/home/arjun/.env')
+# dotenv_path = Path('/home/ubuntu/.env')
+# load_dotenv(dotenv_path=dotenv_path)
 
 
-db_config = {'user' : os.getenv('user'),
-            'password' : os.getenv('password'),
-            'host' : os.getenv('host'),
-            'port' : os.getenv('port')
+# db_config = {'user' : os.getenv('user'),
+#             'password' : os.getenv('password'),
+#             'host' : os.getenv('host'),
+#             'port' : os.getenv('port')
+#             }
+
+db_config = {'user' : "postgres",
+            'password' :"onlilo123",
+            'host' : "database-1.cmqsecpalehs.ap-south-1.rds.amazonaws.com",
+            'port' : "5432"
             }
-
-
