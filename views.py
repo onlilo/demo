be32116a16bd76e45cd9c4f48a083f969d3c4021
Arjun <arjun@onlilo.in>
2022-10-18 14:19:02 +0530

    views.py

--- views.py
+++ views.py
@@ -4,18 +4,17 @@ from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
-from .db import push2dB
+from .pushdata import push2dB
 
 def health(request):
     return JsonResponse({"status":200,"API":"Finally Running"})
 
 @csrf_exempt
 def data2dB(request):
-    print("Arjun")
     try:
         if 'application/json' in request.META['CONTENT_TYPE']:
             json_data= json.loads(request.body)
             result = push2dB(json_data)
-            return JsonResponse({"status":200,"Message":result["message"]})
+            return JsonResponse({"Message":result["message"]})
     except Exception as e:
-            return JsonResponse ({"status_code":500,"message":str(e)})
+            return JsonResponse ({"message":str(e)})
