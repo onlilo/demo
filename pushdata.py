2c237d0db2bfec9e52c0037fa00db7934d1bf565
Arjun <arjun@onlilo.in>
2022-10-20 23:22:11 +0530

    test

--- pushdata.py
+++ pushdata.py
@@ -10,11 +10,11 @@ def push2dB(body):
                                 connect_timeout=20)
         
         cursor = conn.cursor()
-        insert_query="""INSERT INTO public.readings("Ax","Ay","Az","Gx","Gy","Gz","BPM","value") VALUES 
-                        (%(Ax)s,%(Ay)s,%(Az)s,%(Gx)s,%(Gy)s,%(Gz)s,%(BPM)s,%(value)s) 
+        insert_query="""INSERT INTO public.readings("Ax","Ay","Az","Gx","Gy","Gz","BPM","device_id") VALUES 
+                        (%(Ax)s,%(Ay)s,%(Az)s,%(Gx)s,%(Gy)s,%(Gz)s,%(BPM)s,%(device_id)s) 
                         RETURNING id;"""
         records_to_insert= {"Ax":float(body["ax"]),"Ay":float(body["ay"]),"Az":float(body["az"]),"Gx":float(body["gx"]),
-                "Gy":float(body["gy"]),"Gz":float(body["gz"]),"BPM":int(body["BPM"]),"value":int(body["value"])}
+                "Gy":float(body["gy"]),"Gz":float(body["gz"]),"BPM":int(body["BPM"]),"value":int(body["device_id"])}
         cursor.execute(insert_query,records_to_insert)
         conn.commit()
         cursor.close()
